43f7dfa8f87f6549e45714e80399c3c2ab8146b1
shshrukh <shshrukh.arbaz@gmail.com>
2025-09-10 21:38:08 +0500

    feat: Implement user registration with validation and location creation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,37 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import User from "../models/user.models.js";
 import ApiError from "../utils/ApiError.js";    
+import Location from "../models/location.models.js";
 
 
 
 const registerUser = asyncHandler( async ( req, res )=>{
-    const{ }= req.body;
+
+    const{ firstName, middleName, lastName, email, password,  phoneNumber, gender, province, district, city  }= req.body;
+
+    if([firstName, middleName, lastName, email, password, phoneNumber, gender, province, district, city].some( fields => fields.trim() === "")){
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existingUser = await User.findOne({$or :[{email}, {phoneNumber}]});
+    if(existingUser){
+        throw new ApiError(409, "User already exists with this email or phone number");
+    }
+
+    const location = await Location.create({province, district, city});
+
+    const user = await User.create({
+        firstName,
+        middleName,
+        lastName,
+        email,
+        phoneNumber,    
+        password,
+        gender,
+        location : location._id,
+
+    })
+
+
     
 })
